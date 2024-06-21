40e094741ffe6f496e75d22cc4535cb75f90eb4e
rishabhsingh <rishabhsingh4000@gmail.com>
2024-06-22 02:54:07 +0530

    change_password_api

--- restaurant_reservation/account/views.py
+++ restaurant_reservation/account/views.py
@@ -1,5 +1,5 @@
 from .models import User
-from .serializer import UserSerializer,LoginSerializer,UserProfileSerializer
+from .serializer import UserSerializer,LoginSerializer,UserProfileSerializer,ChangePasswordSerializer
 from rest_framework import status,generics
 from rest_framework.decorators import api_view,permission_classes
 from django.views.decorators.csrf import csrf_exempt
@@ -7,6 +7,7 @@ from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
 from django.contrib.auth import authenticate
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from django.contrib.auth import update_session_auth_hash
 
 class UserListView(generics.ListAPIView):  # this the api for the showing the all the user profile in the list form
     queryset = User.objects.all()
@@ -59,3 +60,20 @@ def logout(request):
     
     except(AttributeError, Token.DoesNotExist):
         return Response({"error": "Invalid request"}, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def changePassword(request):
+    user = request.user
+    serializer = ChangePasswordSerializer(data = request.data,context={'request': request})
+
+    if serializer.is_valid():
+        if not user.check_password(serializer.data.get('old_password')):
+            return Response({'old_password': ["Wrong Password"]}, status=status.HTTP_400_BAD_REQUEST)
+        user.set_password(serializer.data.get('new_password'))
+        user.save()
+        update_session_auth_hash(request, user)
+        return Response({"message": "Password updated successfully"}, status=status.HTTP_200_OK)
+    
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
