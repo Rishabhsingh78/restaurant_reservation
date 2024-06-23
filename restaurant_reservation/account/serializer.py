db12952934e63034fc1c6e3a63232ff7fbaa8a4e
rishabhsingh <rishabhsingh4000@gmail.com>
2024-06-24 03:16:59 +0530

    forget_api_with_sending_mail

--- restaurant_reservation/account/serializer.py
+++ restaurant_reservation/account/serializer.py
@@ -3,6 +3,8 @@ from .models import User
 from django.contrib.auth import authenticate
 from rest_framework.exceptions import AuthenticationFailed
 from django.contrib.auth.password_validation import validate_password
+from django.contrib.auth.tokens import default_token_generator
+from django.utils.http import urlsafe_base64_decode
 
 class UserSerializer(serializers.ModelSerializer):  # This serializer is for creating the user 
     class Meta:
@@ -60,3 +62,40 @@ class ChangePasswordSerializer(serializers.Serializer):
         if not user.check_password(data.get('old_password')):
             raise serializers.ValidationError({"old_password": "Wrong password."})
         return data    # here the data is {'old_password': '----', 'new_password': '-------'}
+
+class ForgetPasswordRequestSeriazlier(serializers.Serializer):
+    email = serializers.EmailField()
+
+    def validate_email(self, value):
+        if not User.objects.filter(email = value).exists():
+            return serializers.ValidationError("No user associated with this email address.")
+        return value
+    
+
+# serializers.py
+
+class PasswordResetConfirmSerializer(serializers.Serializer):
+    new_password = serializers.CharField(write_only=True)
+    uidb64 = serializers.CharField()
+    token = serializers.CharField()
+
+    def validate(self, data):
+        try:
+            uid = urlsafe_base64_decode(data['uidb64']).decode()
+            user = User.objects.get(pk=uid)
+        except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+            raise serializers.ValidationError("Invalid token")
+
+        if not default_token_generator.check_token(user, data['token']):
+
+            raise serializers.ValidationError("Invalid token")
+        print("DATA",data)
+        return data
+
+    def save(self, **kwargs):
+        uid = urlsafe_base64_decode(self.validated_data['uidb64']).decode()
+        print("UID",uid)
+        user = User.objects.get(pk=uid)
+        user.set_password(self.validated_data['new_password'])
+        user.save()
+        return user
--- restaurant_reservation/account/urls.py
+++ restaurant_reservation/account/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('users/', views.UserListView.as_view(), name='user-list'),
     path('logout/', views.logout, name='logout'),
     path('change-password/', views.changePassword, name='changepassword'),
+    path('password-reset/', views.request_Password, name='request_password_reset'),
+    path('password-reset-confirm/<uidb64>/<token>/', views.password_reset_confirm, name='password_reset_confirm'),
 ]
