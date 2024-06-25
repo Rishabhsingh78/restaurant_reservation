323ffc141d91ecc5d182e7ca87556402b1accdee
rishabhsingh <rishabhsingh4000@gmail.com>
2024-06-26 02:13:23 +0530

    admin dashboard manage time and slot

--- restaurant_reservation/account/admin.py
+++ restaurant_reservation/account/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 from .models import User,Slot,Reservation
 admin.site.register(User)
 admin.site.register(Reservation)
-admin.site.register(Slot)
+# admin.site.register(Slot)
+
+@admin.register(Slot)
+class SlotAdmin(admin.ModelAdmin):
+    list_display = ('id', 'date', 'start_time', 'end_time', 'location', 'table_number')
+    list_filter = ('date', 'location', 'table_number')
+    search_fields = ('location', 'table_number')
